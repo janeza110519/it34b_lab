6688515e1facc3aa5d84b493ad27bfbac5855655
Janeza Malaya <janezamalaya4@gmail.com>
2026-08-19 15:20:40 +0800

    Added contents for includes/activity-logger.php

---
+++ includes/activity-logger.php
@@ -0,0 +1,32 @@
+<?php
+    function logActivity($pdo,$user_id,$email, $action, $status='success'){
+        try{
+            //Get Client IP Address
+            $ip = 4_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'] ?? 'UNKNOWN';
+
+            // String to Array
+            if(strpos($ip,',') !==false){
+                $ip = trim(explode(',', $ip)[0]);
+            }
+
+            // Get user agent (browser)
+            $user_agent = substr($_SERVER['HTTP_USER_AGENT'] ?? 'Unknown' ,0,255);
+
+            // Application Query #1
+            $stmt = $pdo -> prepare("
+                INSERT INTO activity_logs(
+                    user_id,
+                    user_email,
+                    activity_log_action,
+                    activity_log_action,
+                    activity_log_ip_address,
+                    activity_log_user_agent
+                ) VALUES (?,?,?,?,?,?)
+            ");
+
+        }catch (PDOException $e){
+            error_log("Activity Log Error: " .$e->getMessage());
+            return false;
+        }   
+    }
+?>
